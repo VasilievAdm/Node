8222d4c654ba0dbe981c50a7b0f3be885751fbf4
VasilievAdm <vasiliev.adm@gmail.com>
2022-08-21 12:49:49 +0300

    hw1

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+sync
+const file = fs.readFileSync('index.html', {
+  encoding: 'utf8'
+});
+
+console.log(file);
+
+// fs.promises.readFile('index.html').then(console.log);
+
+// fs.readFile('index.html', (err, data) => {
+//   if (err) {
+//     console.log('error', err)
+//   }
+
+//   console.log(data)
+// })
